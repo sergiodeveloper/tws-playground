1cbb823e9cc6326929e7a5c58933447a7245cb0e
sergiodeveloper <sergio-fh@hotmail.com>
2023-04-17 03:19:40 -0300

    fix: use correct endpoints for server and schema

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ const app = new App({
   target: document.body,
   props: {
     appName: 'TWS Playground',
-    logoPath: './favicon.png',
-    schemaPath: 'http://localhost:3000/tws/schema',
-    serverPath: 'http://localhost:3000/tws',
+    logoPath: 'https://www.unpkg.com/@tws-js/playground@0.1.1/dist/favicon.png',
+    schemaPath:
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      window.TWS_SCHEMA_PATH || '/tws/schema',
+    serverPath:
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      window.TWS_SERVER_PATH || '/tws',
   },
 });
 
